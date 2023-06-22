4c2472073ab0d04b521d3865ba37e8983f248220
itArnaudov <137357257+itArnaudov@users.noreply.github.com>
2023-06-22 21:28:42 +0100

    Create App Gateway in front of ILB with 2x VMs.tf

---
+++ terraform/Azure/App Gateway in front of ILB with 2x VMs.tf
@@ -0,0 +1,145 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "West US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_lb" "internal" {
+  name                = "my-internal-lb"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                          = "internal-ip"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.4"
+  }
+
+  backend_address_pool {
+    name = "backend-pool"
+  }
+
+  probe {
+    name                      = "tcp-probe"
+    protocol                  = "Tcp"
+    port                      = 80
+    interval_in_seconds       = 5
+    number_of_probes          = 2
+  }
+
+  load_balancing_rule {
+    name                       = "lb-rule"
+    frontend_port              = 80
+    backend_port               = 80
+    protocol                   = "Tcp"
+    frontend_ip_configuration = azurerm_lb.internal.frontend_ip_configuration[0].name
+    backend_address_pool       = azurerm_lb.internal.backend_address_pool[0].name
+    probe                      = azurerm_lb.internal.probe[0].name
+  }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "my-public-ip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "my-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example" {
+  name                        = "allow-http"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefixes     = ["*"]
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_application_gateway" "example" {
+  name                = "my-app-gateway"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "app-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.example.id
+  }
+
+  frontend_port {
+    name = "app-gateway-port"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "app-gateway-ip-configuration"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+
+  backend_address_pool {
+    name = "app-gateway-backend-pool"
+  }
+
+  backend_http_settings {
+    name                  = "app-gateway-backend-http-settings"
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = "app-gateway-http-listener"
+    frontend_ip_configuration_name = "app-gateway-ip-configuration"
+    frontend_port_name             = "app-gateway-port"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "app-gateway-routing-rule"
+    rule_type                  = "Basic"
+    http_listener_name         = "app-gateway-http-listener"
+    backend_address_pool_name  = "app-gateway-backend-pool"
+    backend_http_settings_name = "app-gateway-backend-http-settings"
+  }
+}
