a075eb4a9fabb3f8fb750c08e819e355f1079af6
itArnaudov <137357257+itArnaudov@users.noreply.github.com>
2023-06-22 21:26:24 +0100

    Create Internal Load Balancer with Vnet and 2x back end hosts.tf

---
+++ terraform/Azure/Internal Load Balancer with Vnet and 2x back end hosts.tf
@@ -0,0 +1,104 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "West US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_lb" "internal" {
+  name                = "my-internal-lb"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                          = "internal-ip"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.4"
+  }
+
+  backend_address_pool {
+    name = "backend-pool"
+  }
+
+  probe {
+    name                      = "tcp-probe"
+    protocol                  = "Tcp"
+    port                      = 80
+    interval_in_seconds       = 5
+    number_of_probes          = 2
+  }
+
+  load_balancing_rule {
+    name                       = "lb-rule"
+    frontend_port              = 80
+    backend_port               = 80
+    protocol                   = "Tcp"
+    frontend_ip_configuration = azurerm_lb.internal.frontend_ip_configuration[0].name
+    backend_address_pool       = azurerm_lb.internal.backend_address_pool[0].name
+    probe                      = azurerm_lb.internal.probe[0].name
+  }
+}
+
+resource "azurerm_virtual_machine" "example" {
+  count                 = 2
+  name                  = "my-vm-${count.index}"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example[count.index].id]
+  vm_size               = "Standard_DS2_v2"
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "myvm${count.index}"
+    admin_username = "adminuser"
+    admin_password = "P@ssword1234!"
+  }
+
+  os_profile_windows_config {
+    enable_automatic_upgrades = true
+    provision_vm_agent        = true
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  count               = 2
+  name                = "my-nic-${count.index}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip
