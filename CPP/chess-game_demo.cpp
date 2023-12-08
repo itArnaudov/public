1009a5ac13f5d7f252d954086ef19aec39b862ee
iAr_cbre <140811143+Ivan-CBRE@users.noreply.github.com>
2023-12-08 17:20:58 +0000

    proposed_remediations_0.1.1

--- CPP/chess-game_demo.cpp
+++ CPP/chess-game_demo.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-// Define chess piece types
+// Enumerations for different types of chess pieces and their colors
 enum class PieceType {
     Pawn,
     Rook,
@@ -11,42 +11,40 @@ enum class PieceType {
     King
 };
 
-// Define chess piece colors
 enum class PieceColor {
     White,
     Black
 };
 
-// Define a chess piece
+// Structure representing a chess piece
 struct ChessPiece {
     PieceType type;
     PieceColor color;
 };
 
-// Define a chess board
+// Class representing the chessboard
 class ChessBoard {
 private:
     std::vector<std::vector<ChessPiece>> board;
 
 public:
+    // Constructor to initialize the chessboard
     ChessBoard() : board(8, std::vector<ChessPiece>(8)) {
         initializeBoard();
     }
 
-    // Function to initialize the chess board with pieces
+    // Function to set up the initial configuration of the chessboard
     void initializeBoard() {
-        // Initialize pawns
         for (int i = 0; i < 8; ++i) {
             board[1][i] = {PieceType::Pawn, PieceColor::White};
             board[6][i] = {PieceType::Pawn, PieceColor::Black};
         }
 
-        // Initialize other pieces
         initializePieces(0, PieceColor::White);
         initializePieces(7, PieceColor::Black);
     }
 
-    // Function to initialize non-pawn pieces on a row
+    // Function to set up the initial configuration of pieces for a given row and color
     void initializePieces(int row, PieceColor color) {
         board[row][0] = {PieceType::Rook, color};
         board[row][1] = {PieceType::Knight, color};
@@ -58,7 +56,7 @@ public:
         board[row][7] = {PieceType::Rook, color};
     }
 
-    // Function to display the chess board
+    // Function to display the current state of the chessboard
     void displayBoard() {
         for (int i = 0; i < 8; ++i) {
             for (int j = 0; j < 8; ++j) {
@@ -81,7 +79,50 @@ public:
             std::cout << std::endl;
         }
     }
-};
+
+    // Function to check if a position is within the board's bounds
+    bool isValidPosition(int x, int y) const {
+        return x >= 0 && x < 8 && y >= 0 && y < 8;
+    }
+
+    // Function to check if a position is occupied by a piece
+    bool isOccupied(int x, int y) const {
+        return isValidPosition(x, y) && board[x][y].type != PieceType::Pawn;
+    }
+
+    // Function to check if a move from one position to another is valid for a given piece
+    bool isValidMove(int fromX, int fromY, int toX, int toY) const {
+        // Check if positions are valid
+        if (!isValidPosition(fromX, fromY) || !isValidPosition(toX, toY)) {
+            return false;
+        }
+
+        // Check if there is a piece at the starting position
+        if (!isOccupied(fromX, fromY)) {
+            return false;
+        }
+
+        // Check specific move rules based on piece type
+        ChessPiece piece = board[fromX][fromY];
+
+        switch (piece.type) {
+            case PieceType::Pawn:
+                return isValidPawnMove(fromX, fromY, toX, toY);
+            case PieceType::Rook:
+                return isValidRookMove(fromX, fromY, toX, toY);
+            case PieceType::Knight:
+                return isValidKnightMove(fromX, fromY, toX, toY);
+            case PieceType::Bishop:
+                return isValidBishopMove(fromX, fromY, toX, toY);
+            case PieceType::Queen:
+                return isValidQueenMove(fromX, fromY, toX, toY);
+            case PieceType::King:
+                return isValidKingMove(fromX, fromY, toX, toY);
+            default:
+                return false;
+        }
+    }
+
 //
 int main() {
     ChessBoard chessBoard;
