00ff44d19838a31c975d0a09af41dbe4d4902d2f
itAr <137357257+itArnaudov@users.noreply.github.com>
2024-10-03 17:53:33 +0100

    Update set_cloud_module_tag.sh
    trying to fix the depth of variables.tf search string

--- .github/scripts/set_cloud_module_tag.sh
+++ .github/scripts/set_cloud_module_tag.sh
@@ -3,27 +3,51 @@
 # This script is used to ensure the cloud_module tag is set and at the correct version.
 
 # Set vars
+# while getopts p:v: flag
+# do
+#   case "${flag}" in
+#     p) ws_path="$OPTARG";;
+#     v) version="$OPTARG";;
+#   esac
+# done
+
+# rendered_version=$(echo $version | sed 's/\./-/g')
+
+# if test -f "$ws_path/variables.tf"; then
+#   var_file='variables.tf'
+# elif test -f "$ws_path/_variables.tf"; then
+#   var_file='_variables.tf'
+# elif test -f "$ws_path/vars.tf"; then
+#   var_file='vars.tf'
+# elif test -f "$ws_path/_vars.tf"; then
+#   var_file='_vars.tf'
+# else
+#   echo "No variables file found"
+#   exit 1
+# fi
+
+# sed -E -i '/cloud_module/s/"(.*)_.*"/"\1_'$rendered_version'"/' $ws_path/$var_file
+
+#!/bin/bash
+
+# This script is used to ensure the cloud_module tag is set and at the correct version.
+
+# Set vars
 while getopts p:v: flag
 do
-  case "${flag}" in
-    p) ws_path="$OPTARG";;
-    v) version="$OPTARG";;
-  esac
+    case "${flag}" in
+        p) ws_path="$OPTARG";;
+        v) version="$OPTARG";;
+    esac
 done
 
 rendered_version=$(echo $version | sed 's/\./-/g')
 
-if test -f "$ws_path/variables.tf"; then
-  var_file='variables.tf'
-elif test -f "$ws_path/_variables.tf"; then
-  var_file='_variables.tf'
-elif test -f "$ws_path/vars.tf"; then
-  var_file='vars.tf'
-elif test -f "$ws_path/_vars.tf"; then
-  var_file='_vars.tf'
-else
-  echo "No variables file found"
-  exit 1
+var_file=$(find "$ws_path" -maxdepth 2 -type f -name "variables.tf" -o -name "_variables.tf" -o -name "vars.tf" -o -name "_vars.tf")
+
+if [[ -z "$var_file" ]]; then
+    echo "No variables file found"
+    exit 1
 fi
 
-sed -E -i '/cloud_module/s/"(.*)_.*"/"\1_'$rendered_version'"/' $ws_path/$var_file
+sed -E -i '/cloud_module/s/"(.*)_.*"/"\1_'$rendered_version'"/' "$var_file"
