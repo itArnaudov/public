6200f785872db479876c71aed4f268c8e4cc655b
itAr <137357257+itArnaudov@users.noreply.github.com>
2024-10-10 15:23:04 +0100

    Update vars.tf

--- terraform/Azure/vars.tf
+++ terraform/Azure/vars.tf
@@ -1,1 +1,16 @@
 // new file below 
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-b"
+}
+
+variable "network_cidr" {
+  default = "10.127.0.0/20"
+}
+
+variable "network_name" {
+  default = "tf-custom-machine"
+}
